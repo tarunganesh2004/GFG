7df43b5836d30b38fbb485fa34309a1a0806ea00
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2024-08-05 08:34:08 +0530

    4th August

---
+++ 4th_August.java
@@ -0,0 +1,25 @@
+// N meetings in one room
+import java.util.*;
+class nMeetings {
+    public static int maxMeetings(int n, int[] start, int[] end) {
+        int[][] a = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            a[i][0] = start[i];
+            a[i][1] = end[i];
+        }
+        Arrays.sort(a, (x, y) -> x[1] - y[1]);
+        int s = a[0][0];
+        int c = 0;
+        int e = a[0][1];
+        for (int i = 1; i < n; i++) {
+            if (a[i][0] <= e) {
+                continue;
+            } else {
+                c++;
+                e = a[i][1];
+
+            }
+        }
+        return c + 1;
+    }
+}
