48c18c181bcf33e57bd97a541d6fea8e7199d1c4
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-12-28 17:11:38 +0530

    28th_dec

--- DECEMBER/28th_dec.py
+++ DECEMBER/28th_dec.py
@@ -2,38 +2,42 @@
 
 arr=[0,-1,2,-3,1]
 
-def findTriplets(arr):
-    # sort the array
-    arr.sort()
-    n=len(arr)
-    res=[]
+class Solution:
+    def findTriplets(self,arr):
+        n = len(arr)
+        arr = [(arr[i], i) for i in range(n)]
+        arr.sort()
+        ans = []
+        for i in range(n - 2):
+            j, k = i + 1, n - 1
+            while j < k:
+                if arr[i][0] + arr[j][0] + arr[k][0] == 0:
+                    ans.append(list(sorted([arr[i][1], arr[j][1], arr[k][1]])))
+                    # The other option of considering the j++
+                    for u in range(j + 1, k):
+                        if arr[i][0] + arr[u][0] + arr[k][0] == 0:
+                            ans.append(list(sorted([arr[i][1], arr[u][1], arr[k][1]])))
+                    k -= 1
+                elif arr[i][0] + arr[j][0] + arr[k][0] > 0:
+                    k -= 1
+                else:
+                    j += 1
+        return ans
+    
+    def anotherApproach(self,arr):
+        # using Map
+        n=len(arr)
+        res=[]
+        for i in range(n-2):
+            seen={}
+            for j in range(i+1,n):
+                complement=-(arr[i]+arr[j])
+                if complement in seen:
+                    res.append(sorted([seen[complement],i,j]))
+                
+                seen[arr[j]]=j
 
-    for i in range(n):
-        # skip the duplicates for first element
-        if i>0 and arr[i]==arr[i-1]:
-            continue
-
-        # now use two pointers to find the other two elements
-        left=i+1
-        right=n-1
-        while left<right:
-            cur_sum=arr[i]+arr[left]+arr[right]
-            if cur_sum==0:
-                res.append([arr[i],arr[left],arr[right]])
-
-                # skip duplicates for second element
-                while left < right and arr[left] == arr[left - 1]:
-                    left += 1
-                # skip duplicates for third element
-                while left < right and arr[right] == arr[right - 1]:
-                    right -= 1
-
-                left+=1
-                right-=1
-            elif cur_sum<0:
-                left+=1
-            else:
-                right-=1
-    return res
-
-print(findTriplets(arr))
+        return res
+sol = Solution()
+print(sol.findTriplets(arr))
+print(sol.anotherApproach(arr))
