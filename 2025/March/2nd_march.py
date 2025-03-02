df6b81ecbf61bf45a2d81dee9f1aceaf23855947
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-02 10:48:06 +0530

    Add optimized implementation for finding maximums in K sized subarrays using deque

--- 2025/March/2nd_march.py
+++ 2025/March/2nd_march.py
@@ -1,5 +1,8 @@
 # K Sized Subarray Maximum
 
+from collections import deque
+
+
 arr=[1,2,3,1,4,5,2,3,6]
 k=3
 
@@ -10,4 +13,19 @@ def bruteForce(arr,k):
         res.append(max(arr[i:i+k]))
     return res
 
+# optimized approach
+def maxOfSubarray(arr,k):
+    q=deque()
+    res=[]
+    for i in range(len(arr)):
+        while q and arr[i]>arr[q[-1]]:
+            q.pop()
+        q.append(i)
+        if q[0]==i-k:
+            q.popleft()
+        if i>=k-1:
+            res.append(arr[q[0]])
+    return res
+
 print(bruteForce(arr,k))
+print(maxOfSubarray(arr,k))
