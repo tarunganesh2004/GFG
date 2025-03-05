1ad4c116f5ef89f71025bff1b17653b36c614d38
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-05 06:37:07 +0530

    Add optimized implementation for finding the longest increasing subsequence using binary search

--- 2025/March/4th_march.py
+++ 2025/March/4th_march.py
@@ -12,3 +12,15 @@ def bruteForce(arr):
                 dp[i]=max(dp[i],dp[j]+1)
     return max(dp)
 
+# using binary search
+def optimized(arr):
+    from bisect import bisect_left
+    n=len(arr)
+    dp=[0]*n
+    length=0
+    for num in arr:
+        i=bisect_left(dp,num,0,length)
+        dp[i]=num
+        if i==length:
+            length+=1
+    return length
