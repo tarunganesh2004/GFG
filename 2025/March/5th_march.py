f0497f49e2345b511f7ffeb392172c7b129e58da
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-05 06:45:21 +0530

    Add optimized implementation for finding the longest string chain

--- 2025/March/5th_march.py
+++ 2025/March/5th_march.py
@@ -11,4 +11,35 @@ def longestStrChain(words):
             dp[word]=max(dp[word],dp[word[:i]+word[i+1:]]+1)
     return max(dp.values())
 
+# optimized
+def optimized(words):
+    from collections import defaultdict
+    s=set(words)
+    ws=defaultdict(set)
+
+    for w in words:
+        for i in range(len(w)):
+            e=w[:i]+w[i+1:]
+            if e in s:
+                ws[e].add(w)
+    
+    words.sort(key=len)
+    cache=defaultdict(int)
+
+    def dfs(w):
+        if w in cache:
+            return cache[w]
+        
+        r=1
+        for c in ws[w]:
+            r=max(r,dfs(c)+1)
+        cache[w]=r
+        return r
+    
+    res=0
+    for w in words:
+        res=max(res,dfs(w))
+    return res
+
 print(longestStrChain(words)) # 4
+print(optimized(words)) # 4
