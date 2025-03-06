b1d9e94483adfd7089e620099d593718347ccb80
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-06 19:25:08 +0530

    Add memoization implementation for longest common subsequence

--- 2025/March/6th_march.py
+++ 2025/March/6th_march.py
@@ -11,4 +11,22 @@ def lcs_recursive(s1,s2):
         return 1+lcs_recursive(s1[1:],s2[1:])
     return max(lcs_recursive(s1[1:],s2),lcs_recursive(s1,s2[1:]))
 
+# memoization
+def lcs_memoization(s1,s2):
+    cache={}
+    def helper(i,j):
+        if i>=len(s1) or j>=len(s2):
+            return 0
+        if (i,j) in cache:
+            return cache[(i,j)]
+        if s1[i]==s2[j]:
+            cache[(i,j)]=1+helper(i+1,j+1)
+        else:
+            cache[(i,j)]=max(helper(i+1,j),helper(i,j+1))
+        return cache[(i,j)]
+    return helper(0,0)
+
+
+
 print(lcs_recursive(s1,s2)) # 3
+print(lcs_memoization(s1,s2)) # 3
