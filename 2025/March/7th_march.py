cf4f861806d0e18d6671e28b660e3f94865c4b72
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-07 16:46:59 +0530

    Add dynamic programming implementation for longest palindromic subsequence

--- 2025/March/7th_march.py
+++ 2025/March/7th_march.py
@@ -32,6 +32,24 @@ def lps_memo(s,i,j,memo):
         memo[(i,j)]=max(lps_memo(s,i+1,j,memo),lps_memo(s,i,j-1,memo))
     return memo[(i,j)]
 
+# Tabulation - O(n^2) bottom up
+def lps_dp(s):
+    n=len(s)
+    dp=[[0]*n for _ in range(n)]
+    for i in range(n):
+        dp[i][i]=1 # single character is always a palindrome
+
+    # start from the end
+    for i in range(n-1,-1,-1):
+        for j in range(i+1,n):
+            if s[i]==s[j]:
+                dp[i][j]=2+dp[i+1][j-1]
+            else:
+                dp[i][j]=max(dp[i+1][j],dp[i][j-1])
+    return dp[0][-1]
+
+
     
 print(lps_recursive(s,0,len(s)-1)) # 7
 print(lps_memo(s,0,len(s)-1,{})) # 7
+print(lps_dp(s)) # 7
