b6afbf6405452795a6411246dcff1f9a124a135e
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-07 16:41:50 +0530

    Add memoization implementation for longest palindromic subsequence

--- 2025/March/7th_march.py
+++ 2025/March/7th_march.py
@@ -15,6 +15,23 @@ def lps_recursive(s,i,j): # O(2^n)
     else:
         return max(lps_recursive(s,i+1,j),lps_recursive(s,i,j-1))
     
+# Memoization - O(n^2)
+def lps_memo(s,i,j,memo):
+    if i==j:
+        return 1
+    if i>j:
+        return 0
+    
+    # check if the value is already calculated
+    if (i,j) in memo:
+        return memo[(i,j)]
+    
+    if s[i]==s[j]:
+        memo[(i,j)]=2+lps_memo(s,i+1,j-1,memo)
+    else:
+        memo[(i,j)]=max(lps_memo(s,i+1,j,memo),lps_memo(s,i,j-1,memo))
+    return memo[(i,j)]
 
     
 print(lps_recursive(s,0,len(s)-1)) # 7
+print(lps_memo(s,0,len(s)-1,{})) # 7
