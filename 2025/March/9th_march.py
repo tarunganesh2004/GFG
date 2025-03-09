23574c7de7d420004d7b2699b4666c90078f42cf
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-09 09:14:11 +0530

    Add expand around center approach to count palindromic substrings of length greater than 2

--- 2025/March/9th_march.py
+++ 2025/March/9th_march.py
@@ -15,6 +15,26 @@ def brute_force(s):
                 count+=1
     return count
 
+# it can be solved in O(n^2) time complexity using dynamic programming, but space complexity will be O(n^2)
+
+# better approach is expand around center
+def countPalindromes(s):
+    def expand(s,left,right):
+        n=len(s)
+        count=0
+        while left>=0 and right<n and s[left]==s[right]:
+            if right-left+1>=2: # length>2
+                count+=1
+            left-=1
+            right+=1
+        return count
+    n=len(s)
+    count=0
+    for i in range(n):
+        count+=expand(s,i,i) # odd length
+        count+=expand(s,i,i+1)
+    return count
 
 
 print(brute_force(s1))
+print(countPalindromes(s1)) # 4
