23143391b93b22ecdb37b754399bf97d015cfe30
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-10 09:37:04 +0530

    Add tabulation implementation for edit distance

--- 2025/March/10th_march.py
+++ 2025/March/10th_march.py
@@ -32,5 +32,24 @@ def memoiz(s1,s2):
     dp=[[-1 for i in range(len(s2)+1)] for j in range(len(s1)+1)]
     return helper(s1,s2,len(s1),len(s2),dp)
 
+# tabulation
+def dp(s1,s2):
+    m=len(s1)
+    n=len(s2)
+    dp=[[0 for i in range(n+1)] for j in range(m+1)]
+    for i in range(m+1):
+        for j in range(n+1):
+            if i==0:
+                dp[i][j]=j
+            elif j==0:
+                dp[i][j]=i
+            elif s1[i-1]==s2[j-1]:
+                dp[i][j]=dp[i-1][j-1]
+            else:
+                dp[i][j]=1+min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1])
+    return dp[m][n]
+
+
 print(brute_force(s1,s2)) # 1
 print(memoiz(s1,s2)) # 1
+print(dp(s1,s2)) # 1
