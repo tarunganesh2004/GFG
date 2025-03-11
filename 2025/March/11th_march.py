d44e05f3c3aa3fb6eb3ecc0e36e549208ea195e9
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-11 22:18:32 +0530

    Add memoization implementation for calculating ways to reach the nth stair

--- 2025/March/11th_march.py
+++ 2025/March/11th_march.py
@@ -12,4 +12,19 @@ def recursion(n):
         return f(n-1)+f(n-2)+f(n-3)
     return f(n)
 
+# memoization
+def memoi(n):
+    def f(n,memo):
+        if n==0:
+            return 1
+        if n<0:
+            return 0
+        if memo[n]!=-1:
+            return memo[n]
+        
+        memo[n]=f(n-1,memo)+f(n-2,memo)+f(n-3,memo)
+        return memo[n]
+    memo=[-1 for i in range(n+1)]
+    return f(n,memo)
+
 print(recursion(n)) # 1
