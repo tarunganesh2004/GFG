7447d3dcfa055a39b3e7713d735a6a378a51d9f7
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-12 09:23:39 +0530

    Add memoization implementation for minimum cost calculation in climbing stairs

--- 2025/March/12th_march.py
+++ 2025/March/12th_march.py
@@ -13,4 +13,21 @@ def recursion(cost):
         return cost[i]+min(min_cost(i-1,cost),min_cost(i-2,cost))
     return min(min_cost(len(cost)-1,cost),min_cost(len(cost)-2,cost))
 
+# memoization
+def memoi(cost):
+    def min_cost(i,cost,memo):
+        if i<0:
+            return 0
+        if i==0 or i==1:
+            return cost[i]
+        if memo[i]!=-1:
+            return memo[i]
+        
+        memo[i]=cost[i]+min(min_cost(i-1,cost,memo),min_cost(i-2,cost,memo))
+        return memo[i]
+    n=len(cost)
+    memo=[-1]*n
+    return min(min_cost(len(cost)-1,cost,memo),min_cost(len(cost)-2,cost,memo))
+
 print(recursion(cost))
+print(memoi(cost)) # 15
