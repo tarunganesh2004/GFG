7b22d600b4056391dbcf476a254bf481ddfc0f4a
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-12 09:21:18 +0530

    Add recursive implementation to calculate minimum cost of climbing stairs

---
+++ 2025/March/12th_march.py
@@ -0,0 +1,16 @@
+# Min Cost Climbing Stairs
+
+cost=[10,15,20] # ways are from 10 to 20(cost=10+20=30) or start at 15 move 2 steps to goal(cost=15)
+
+# recursion
+def recursion(cost):
+    def min_cost(i,cost):
+        if i<0:
+            return 0
+        if i==0 or i==1:
+            return cost[i]
+        
+        return cost[i]+min(min_cost(i-1,cost),min_cost(i-2,cost))
+    return min(min_cost(len(cost)-1,cost),min_cost(len(cost)-2,cost))
+
+print(recursion(cost))
