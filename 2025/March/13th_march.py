866988658f184fb8e2ea706ff25d2698701c2eb8
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-13 17:25:38 +0530

    Add bottom-up dynamic programming implementation for 0-1 Knapsack problem

--- 2025/March/13th_march.py
+++ 2025/March/13th_march.py
@@ -32,5 +32,18 @@ def memoization(cap,wt,val):
     memo=[[-1]*(cap+1) for _ in range(n)]
     return knap(n-1,cap,wt,val,memo)
 
+# DP - Bottom Up
+def dp(cap,wt,val):
+    n=len(wt)
+    dp=[[0]*(cap+1) for _ in range(n+1)]
+    for i in range(1,n+1):
+        for j in range(1,cap+1): # j is the capacity
+            if wt[i-1]>j:
+                dp[i][j]=dp[i-1][j] # skip
+            else:
+                dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j])
+    return dp[n][cap]
+
+
 print(recursion(cap,wt,val)) # 3
 print(memoization(cap,wt,val)) # 3
