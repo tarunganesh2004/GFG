c2b09c77745ea37af724cef68fc3442be6de5f2a
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-13 17:24:34 +0530

    Add memoization implementation for 0-1 Knapsack problem

--- 2025/March/13th_march.py
+++ 2025/March/13th_march.py
@@ -15,4 +15,21 @@ def recursion(cap,wt,val):
             return max(val[i]+knap(i-1,cap-wt[i],wt,val),knap(i-1,cap,wt,val)) # include or skip
     return knap(len(wt)-1,cap,wt,val)
 
+# memoization
+def memoization(cap,wt,val):
+    def knap(i,cap,wt,val,memo):
+        if i<0 or cap==0:
+            return 0
+        if memo[i][cap]!=-1:
+            return memo[i][cap]
+        if wt[i]>cap:
+            memo[i][cap]=knap(i-1,cap,wt,val,memo)
+            return memo[i][cap]
+        else:
+            memo[i][cap]=max(val[i]+knap(i-1,cap-wt[i],wt,val,memo),knap(i-1,cap,wt,val,memo))
+            return memo[i][cap]
+    n=len(wt)
+    memo=[[-1]*(cap+1) for _ in range(n)]
+    return knap(n-1,cap,wt,val,memo)
+
 print(recursion(cap,wt,val)) # 3
