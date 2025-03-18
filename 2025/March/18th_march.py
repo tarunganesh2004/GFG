1031059c347f265fac51bb144faa893b1d17a063
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-18 10:48:38 +0530

    Add dynamic programming implementation for subset sum problem

--- 2025/March/18th_march.py
+++ 2025/March/18th_march.py
@@ -78,6 +78,26 @@ def subsetSumMemoi(arr):
     
     return recur(0,total_sum//2,{})
 
+# dp
+def subsetSumDP(arr):
+    total_sum=sum(arr)
+    if total_sum%2!=0:
+        return False
+    n=len(arr)
+    target=total_sum//2
+    dp=[[False]*(target+1) for _ in range(n+1)]
+    for i in range(n+1):
+        dp[i][0]=True
+    for i in range(1,n+1):
+        for j in range(1,target+1):
+            if j-arr[i-1]>=0:
+                dp[i][j]=dp[i-1][j] or dp[i-1][j-arr[i-1]]
+            else:
+                dp[i][j]=dp[i-1][j]
+    return dp[n][target]
+
+print(subsetSumDP(arr))
+
 print(bruteForce(arr))
 print(bruteForceMemoi(arr))
 print(subsetSumMemoi(arr))
