0fcda95a03189aa4e0b553432278c5125e22e9e9
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-21 15:15:02 +0530

    Add memoization implementation for brute force algorithm using lru_cache

--- 2025/March/21st_march.py
+++ 2025/March/21st_march.py
@@ -16,4 +16,23 @@ def brute_force(arr):
     
     return recur(0,len(arr))
 
+# using lru_cache
+def brute_force_memoi(arr):
+    from functools import lru_cache
+    arr=tuple(arr)
+
+    @lru_cache(None)
+    def recur(cur_idx,n):
+        if cur_idx>=n:
+            return 0
+        
+        # include current
+        include=arr[cur_idx]+recur(cur_idx+2,n)
+        # exclude current
+        exclude=recur(cur_idx+1,n)
+        return max(include,exclude)
+    
+    return recur(0,len(arr))
+
 print(brute_force(arr)) # 15
+print(brute_force_memoi(arr)) # 15
