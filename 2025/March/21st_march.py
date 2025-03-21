479538ef07494cbcae6e54f2fe8d7a40e7215978
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-21 15:18:47 +0530

    Add dynamic programming implementation for the problem in March 21st script

--- 2025/March/21st_march.py
+++ 2025/March/21st_march.py
@@ -54,6 +54,20 @@ def memoization(arr):
     
     return recurMemo(0,len(arr),memo)
 
+# Dp
+def dp(arr):
+    n=len(arr)
+    dp=[0]*(n+2)
+    dp[n]=0
+    dp[n+1]=0
+
+    for i in range(n-1,-1,-1):
+        dp[i]=max(arr[i]+dp[i+2],dp[i+1])
+    
+    return dp[0]
+
+
 print(brute_force(arr)) # 15
 print(brute_force_memoi(arr)) # 15
 print(memoization(arr)) # 15
+print(dp(arr)) # 15
