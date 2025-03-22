b5e15d39e4e922c0eb86ef412c58cc9e115f542c
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-22 16:42:56 +0530

    Add memoization function for optimized brute force algorithm

--- 2025/March/22nd_march.py
+++ 2025/March/22nd_march.py
@@ -46,5 +46,32 @@ def brute_force_memoi(arr):
 
     return max(case1,case2)
 
+# memoization
+def memoization(arr):
+    memo={}
+    def recurMemo(cur_idx,n,memo):
+        if cur_idx>=n:
+            return 0
+        
+        if cur_idx in memo:
+            return memo[cur_idx]
+        
+        # include current
+        include=arr[cur_idx]+recurMemo(cur_idx+2,n,memo)
+        # exclude current
+        exclude=recurMemo(cur_idx+1,n,memo)
+
+        memo[cur_idx]=max(include,exclude)
+        return memo[cur_idx]
+    
+    n=len(arr)
+    # case 1 rob from house [0] to house [n-2](excluding last house)
+    case1=recurMemo(0,n-1,memo)
+    # case 2 rob from house [1] to house [n-1](excluding first house)
+    case2=recurMemo(1,n,memo)
+
+    return max(case1,case2)
+
 print(brute_force(arr))
 print(brute_force_memoi(arr)) # 3
+print(memoization(arr)) # 3
