f30cbe42b683420e11b3a945df083a4b0496458b
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-24 16:35:42 +0530

    Add memoization function for matrix chain multiplication

--- 2025/March/24th_march.py
+++ 2025/March/24th_march.py
@@ -38,7 +38,29 @@ def memoi_brute_force(arr):
     
     return recur(1,len(arr)-1)
 
+# memoization
+def memoization(arr):
+    def recurMemo(start,end,memo):
+        if start==end:
+            return 0
+        
+        if (start,end) in memo:
+            return memo[(start,end)]
+        
+        min_cost=float("inf")
+        for k in range(start,end):
+            left=recurMemo(start,k,memo)
+            right=recurMemo(k+1,end,memo)
+            cost=left+right+(arr[start-1]*arr[k]*arr[end])
+            min_cost=min(min_cost,cost)
+        
+        memo[(start,end)]=min_cost
+        return memo[(start,end)]
+    
+    return recurMemo(1,len(arr)-1,{})
+
 
 
 print(brute_force(arr)) # 20
 print(memoi_brute_force(arr)) # 20
+print(memoization(arr)) # 20
