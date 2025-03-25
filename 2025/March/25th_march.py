ae8be087f7f4b0d3b9f7bb71aba5adcc346a13dc
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-25 13:31:12 +0530

    Add memoization function for Boolean Parenthesization Problem

--- 2025/March/25th_march.py
+++ 2025/March/25th_march.py
@@ -82,7 +82,49 @@ def memoi_brute_force(s):
     
     return recur(0,len(s)-1,True)
 
-
+# Memoization
+def memoization(s):
+    def recurMemo(i,j,isTrue,memo):
+        if i>j:
+            return False
+        
+        if i==j:
+            if isTrue:
+                return s[i]=="T"
+            else:
+                return s[i]=="F"
+        
+        if (i,j,isTrue) in memo:
+            return memo[(i,j,isTrue)]
+        
+        ans=0
+        for k in range(i+1,j,2):
+            lt= recurMemo(i,k-1,True,memo)
+            lf= recurMemo(i,k-1,False,memo)
+            rt= recurMemo(k+1,j,True,memo)
+            rf= recurMemo(k+1,j,False,memo)
+            
+            if s[k]=="&":
+                if isTrue:
+                    ans+=lt*rt
+                else:
+                    ans+=lt*rf+lf*rt+lf*rf
+            elif s[k]=="|":
+                if isTrue:
+                    ans+=lt*rt+lt*rf+lf*rt
+                else:
+                    ans+=lf*rf
+            elif s[k]=="^":
+                if isTrue:
+                    ans+=lt*rf+lf*rt
+                else:
+                    ans+=lt*rt+lf*rf
+        
+        memo[(i,j,isTrue)]=ans
+        return ans
+    
+    return recurMemo(0,len(s)-1,True,{})
 
 print(brute_force(s)) # 4
 print(memoi_brute_force(s)) # 4
+print(memoization(s)) # 4
