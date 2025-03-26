4685f14ffcdb633b0f7db8fd73d2b64184256c52
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-03-26 16:22:35 +0530

    Add memoized recursive solution for string segmentation

--- 2025/March/26th_march.py
+++ 2025/March/26th_march.py
@@ -14,4 +14,22 @@ def recur(s):
     
     return False
 
+# using lru_cache
+def memoi_recur(s):
+    from functools import lru_cache
+    @lru_cache(None)
+    def recur(s):
+        if s=="":
+            return True
+        
+        for i in range(1,len(s)+1):
+            if s[:i] in dict and recur(s[i:]):
+                return True
+        
+        return False
+    
+    return recur(s)
+
+
 print(recur(s))
+print(memoi_recur(s))
