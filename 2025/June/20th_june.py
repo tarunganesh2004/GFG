1da39f89338c959b506527f153cb93299c88d956
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-06-20 10:57:21 +0530

    Add implementation for grouping balls by sequence

---
+++ 2025/June/20th_june.py
@@ -0,0 +1,31 @@
+# Group Balls By Sequence
+
+arr=[10,1,2,11]
+k=2
+
+def groupBalls(arr, k):
+    from collections import Counter
+    n=len(arr)
+    if n % k != 0:
+        return False  # Cannot group if total balls are not divisible by k
+    
+    arr.sort()
+    d=dict(Counter(list(arr)))
+
+    for item in arr:
+        if d[item]==0:
+            continue
+
+        temp=k
+        cur=item
+
+        while temp>0:
+            if cur not in d or d[cur]==0:
+                return False
+            d[cur]-=1
+            cur+=1
+            temp-=1
+    return True
+
+
+print(groupBalls(arr, k))  # Output: True
