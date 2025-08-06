03611bf57ddc98e64a6c9cd1ea321edf5aa7c7fe
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-08-06 17:01:20 +0530

    Add implementation for Roman numeral to decimal converter in Python with a corresponding Java snippet

---
+++ 2025/August/6th_august.py
@@ -0,0 +1,44 @@
+
+s="IX"
+
+def romanToDecimal(s):
+    map={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+
+    res=0
+    i=0
+    while i<len(s):
+        if i+1<len(s) and map[s[i]]<map[s[i+1]]:
+            res+=map[s[i+1]]-map[s[i]]
+            i+=1
+        else:
+            res+=map[s[i]]
+        i+=1
+    return res
+
+print(romanToDecimal(s))
+
+"""
+Java Snippet:
+public static int romanToInt(String s){
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        int i = 0;
+        int res = 0;
+        while (i < s.length()) {
+            if (i + 1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                res += map.get(s.charAt(i + 1)) - map.get(s.charAt(i));
+                i++;
+            } else {
+                res += map.get(s.charAt(i));
+                i++;
+            }
+
+        }
+        return res;
+    }
+"""
