ea39899c772aa3a053910f87e6c61fd4bd2afe84
tarunganesh2004 <enstarunganesh@gmail.com>
2025-08-26 12:30:15 +0530

    Add implementation to check if a string is a subsequence of another

---
+++ 2025/August/26th_august.py
@@ -0,0 +1,16 @@
+# Check if a String is Subsequence of Other
+
+s1="AXY"
+s2="YADXCP"
+
+def is_subsequence(s1,s2):
+    left,right=0,0
+    while left<len(s1) and right<len(s2):
+        if s1[left]==s2[right]:
+            left+=1
+            right+=1
+        else:
+            right+=1
+    return left==len(s1)
+
+print(is_subsequence(s1,s2))
