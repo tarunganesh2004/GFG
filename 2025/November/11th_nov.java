c3779314bbfa8ae673203e27f3fa9e3f6b6f8769
tarunganesh2004 <enstarunganesh@gmail.com>
2025-11-11 09:57:58 +0530

    Add implementation for Shortest Common Supersequence problem

---
+++ 2025/November/11th_nov.java
@@ -0,0 +1,21 @@
+// Shortest Common Supersequence
+// import java.util.*;
+
+class Solution {
+    public static int minSuperSeq(String s1, String s2) {
+        // code here
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return m + n - dp[m][n];
+    }
+}
